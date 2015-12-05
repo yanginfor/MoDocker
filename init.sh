842e8331a3c7d84bb24adc681deb6ca4bbf822f7
yanginfor <yanginfor@users.noreply.github.com>
2015-12-06 01:02:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 usermod -s /bin/bash www-data
 chown -R  www-data:www-data /var/www
 php /cldata/init.php
-#chmod -R 777 /var/www
+chmod -R 777 /var/www
 
 service apache2 start
 su -c '/usr/bin/aria2c --conf-path=/cldata/aria2.conf' www-data
